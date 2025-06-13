ff639673348a97e5719c7e960d0cbe6814ad0fb0
Erik Jimenez <erikedu5@gmail.com>
2025-06-13 17:59:14 -0600

    fix

--- src/main/java/com/meztlitech/agrobitacora/config/SecurityConfiguration.java
+++ src/main/java/com/meztlitech/agrobitacora/config/SecurityConfiguration.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -96,5 +97,10 @@ public class SecurityConfiguration {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
             throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
     }
 }
--- src/main/java/com/meztlitech/agrobitacora/controller/CropController.java
+++ src/main/java/com/meztlitech/agrobitacora/controller/CropController.java
@@ -9,7 +9,16 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 
 @RestController
@@ -33,8 +42,8 @@ public class CropController {
         return ResponseEntity.ok(cropService.getById(id, token));
     }
 
-    @PutMapping()
-    public ResponseEntity<CropEntity> update(@Valid @RequestBody CropDto cropDto, Long id, @RequestHeader(value = "Authorization") final String token){
+    @PutMapping("/{id}")
+    public ResponseEntity<CropEntity> update(@Valid @RequestBody CropDto cropDto, @PathVariable Long id, @RequestHeader(value = "Authorization") final String token){
         return ResponseEntity.ok(cropService.update(cropDto, id, token));
     }
 
