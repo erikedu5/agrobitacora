bce5a26d770200aeb2a4624a79aba7136af7c21b
Erik Jimenez <erikedu5@gmail.com>
2025-07-26 23:32:06 -0600

    agregar licensias

--- src/main/resources/static/js/admin-licenses.js
+++ src/main/resources/static/js/admin-licenses.js
@@ -9,14 +9,24 @@ App.registerEntity('adminlicenses', {
         <td>${l.studentTotal}</td>
         <td>${l.decryptedText}</td>
         <td>${l.expirationDate}</td>
-        <td><button class="btn btn-sm btn-success send-wa">Enviar</button></td>
+        <td>
+            <button class="btn btn-sm btn-success send-wa">Enviar Erik</button>
+            <button class="btn btn-sm btn-success send-wa1">Enviar Jesus</button>
+        </td>
     </tr>`,
     afterLoad: () => {
         document.querySelectorAll('.send-wa').forEach(btn => {
             btn.addEventListener('click', () => {
                 const item = JSON.parse(decodeURIComponent(btn.closest('tr').dataset.item));
                 const payload = JSON.stringify({ CipherText: item.cipherText, Iv: item.iv, Key: item.key });
-                window.open(`https://wa.me/?text=${encodeURIComponent(payload)}`, '_blank');
+                window.open(`https://wa.me/5217228259581?text=${encodeURIComponent(payload)}`, '_blank');
+            });
+        });
+        document.querySelectorAll('.send-wa1').forEach(btn => {
+            btn.addEventListener('click', () => {
+                const item = JSON.parse(decodeURIComponent(btn.closest('tr').dataset.item));
+                const payload = JSON.stringify({ CipherText: item.cipherText, Iv: item.iv, Key: item.key });
+                window.open(`https://wa.me/5217221833593?text=${encodeURIComponent(payload)}`, '_blank');
             });
         });
     }
